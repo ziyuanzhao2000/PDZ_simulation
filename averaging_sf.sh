bb99b78496945cc971a377f36e5d54ecb8f7627b
Ziyuan Zhao <ziyuanzhao@college.harvard.edu>
2022-01-06 16:44:59 -0800

    Fix scripts

--- averaging_sf.sh
+++ averaging_sf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -c 1                # Number of cores (-c)
-#SBATCH -t 0-01:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-00:10          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -serial_requeue
 #SBATCH --mem=4G           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o averaging_sf.out
 #SBATCH -e averaging_sf.err
 source ~/.bashrc
 conda activate openmm # need to have reciprocalspaceship lib installed
-python averaging_sf.py -d Dec15_liganded_PDZ -i traj_snapshot.h5 -c 0 -n 100 -v
+python averaging_sf.py -d Dec15_liganded_PDZ -i traj_snapshot -c ${SLURM_ARRAY_TASK_ID} -n 100 -v
