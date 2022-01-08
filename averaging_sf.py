be56f8b13b972b63a79458312ea731c3c842654d
Ziyuan Zhao <ziyuanzhao@college.harvard.edu>
2022-01-08 11:37:53 -0800

    Updated averaging_sf.py script

--- averaging_sf.py
+++ averaging_sf.py
@@ -5,7 +5,7 @@ import sys, os, getopt
 
 # parse cmdline args in a C-like manner
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "vDd:i:c:n:")
+    opts, args = getopt.getopt(sys.argv[1:], "vDd:i:c:n:C:")
 except getopt.GetoptError as err:
     print(err)
     sys.exit(1)
@@ -16,6 +16,7 @@ input_name = "traj_snapshot"
 dirname = ""
 verbose = False
 dryrun = False
+chainwise = False
 
 for o, a in opts:
     if o == "-v":
@@ -30,6 +31,8 @@ for o, a in opts:
         chain_id = int(a)
     elif o == "-n":
         n_frames = int(a)
+    elif o == "-C":
+        chainwise = True
 
 
 if dirname != "":
@@ -41,7 +44,10 @@ if dirname != "":
 
 
 try:
-    dataset = rs.read_mtz(f"{input_name}_0_{chain_id}.mtz")
+    if chainwise:
+        dataset = rs.read_mtz(f"{input_name}_0_{chain_id}.mtz")
+    else:
+        dataset = rs.read_mtz(f"{input_name}_0.mtz")
     n_reflections = dataset.shape[0]
 except:
     print("Can't load the mtz file for the designated chain at frame 0!")
@@ -51,7 +57,10 @@ complex_reflections = np.zeros(n_reflections, dtype='complex128')
 
 # sum up complex reflections and then take average
 for frame in range(n_frames):
-    dataset = rs.read_mtz(f"{input_name}_{frame}_{chain_id}.mtz")
+    if chainwise:
+        dataset = rs.read_mtz(f"{input_name}_{frame}_{chain_id}.mtz")
+    else:
+        dataset = rs.read_mtz(f"{input_name}_{frame}.mtz")
     complex_reflections += np.array([amp*np.exp(np.pi*phase/180 * 1j) for [amp, phase] in dataset.to_numpy()])
     if verbose:
         print(f"Finished processing frame {frame}")
@@ -62,6 +71,10 @@ complex_reflections /= n_frames
 # convert back to polar form and bind the composite indices
 dataset[:] = np.stack([np.abs(complex_reflections), np.angle(complex_reflections) / np.pi * 180]).T
 dataset.infer_mtz_dtypes(inplace = True)
-dataset.write_mtz(f"{input_name}_avg_{chain_id}.mtz")
+if chainwise:
+    dataset.write_mtz(f"{input_name}_avg_{chain_id}.mtz")
+else:
+    dataset.write_mtz(f"{input_name}_avg.mtz")
+
 if verbose:
     print("Wrote average structural factor to the .mtz file!")
